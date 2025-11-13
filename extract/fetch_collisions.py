35375aebdadf2c1672b51f4e1b12ecdbf10782b0
Liz H <erhowell15@gmail.com>
2025-11-13 12:50:24 -0700

    update days

--- extract/fetch_collisions.py
+++ extract/fetch_collisions.py
@@ -3,25 +3,20 @@ import pandas as pd
 import os
 
 # API endpoints for NYC Motor Vehicle Collisions
-CRASH_API_URL = (
-    "https://data.cityofnewyork.us/resource/h9gi-nx95.json"
-    "?$limit=50000&$where=crash_date>'2024-11-01T00:00:00'"
-)
-CRASH_VEHICLE_API_URL = (
-    "https://data.cityofnewyork.us/resource/bm4k-52h4.json"
-    "?$limit=50000&$where=crash_date>'2024-11-01T00:00:00'"
-)
-CRASH_PERSONS_API_URL = (
-    "https://data.cityofnewyork.us/resource/f55k-p6yu.json"
-    "?$limit=50000&$where=crash_date>'2024-11-01T00:00:00'"
-)
+CRASH_COLLISION_ID = 'h9gi-nx95'
+CRASH_VEHICLE_ID = 'bm4k-52h4'
+CRASH_PERSONS_ID = 'f55k-p6yu'
 
-def fetch_data(url, name):
+
+def fetch_data(id, name, limit=100000):
     print(f"Fetching {name} from NYC Open Data...")
+    url = (
+        f"https://data.cityofnewyork.us/resource/{id}.json"
+        f"?$limit={limit}&$where=crash_date>='2024-01-01T00:00:00' AND crash_date<'2025-01-01T00:00:00'"
+    )
     resp = requests.get(url)
     resp.raise_for_status()
     df = pd.DataFrame(resp.json())
-    print(f"✅ Fetched {len(df)} rows for {name}")
     return df
 
 if __name__ == "__main__":
@@ -29,13 +24,13 @@ if __name__ == "__main__":
     os.makedirs("data/raw", exist_ok=True)
 
     # Fetch all three datasets
-    df_crashes = fetch_data(CRASH_API_URL, "crashes")
+    df_crashes = fetch_data(CRASH_COLLISION_ID, "crashes")
     df_crashes.to_csv("data/raw/data_raw_collisions.csv", index=False)
 
-    df_vehicles = fetch_data(CRASH_VEHICLE_API_URL, "vehicles")
+    df_vehicles = fetch_data(CRASH_VEHICLE_ID, "vehicles", 200000)
     df_vehicles.to_csv("data/raw/data_raw_vehicles.csv", index=False)
 
-    df_persons = fetch_data(CRASH_PERSONS_API_URL, "persons")
+    df_persons = fetch_data(CRASH_PERSONS_ID, "persons", 500000)
     df_persons.to_csv("data/raw/data_raw_persons.csv", index=False)
 
     print(f"\n=== Summary ===")
